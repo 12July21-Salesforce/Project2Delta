8f07b87d318c1e204b4b55789dbaaafa7fffc7bf
Michael <michaelerickson98@gmail.com>
2021-08-25 11:57:43 -0400

    Got rid of LSBAItem and replaced it with a Lightning Datatable

--- force-app/main/default/aura/LSBAList/LSBAListController.js
+++ force-app/main/default/aura/LSBAList/LSBAListController.js
@@ -1,5 +1,7 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+    doInit : function(component, event, helper) {
+        component.set('v.columns', [
+            {label: 'Lot Name', fieldName: 'Name', type: 'text'}
+            ]);
+        }
 })
